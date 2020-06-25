70d8f3b0f5f673593264f7008224402b5b42bdf5
Rizky RPJ. Putra Junior <rizky.junior@moonlay.jakarta>
2020-06-25 23:31:31 +0700

    add back button in game over and profile

--- Assets/Scripts/GameOverUI.cs
+++ Assets/Scripts/GameOverUI.cs
@@ -1,9 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameOverUI : MonoBehaviour
 {
+
+    [SerializeField]
+    private Button m_BackBtn;
+    void Start()
+    {
+        m_BackBtn.onClick.AddListener(ClickBackButton);
+    }
     public void Show()
     {
         gameObject.SetActive(true);
+    }
+
+    private void ClickBackButton()
+    {
+        Console.WriteLine("kepencet anjing");
+        // reload the current scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        // load the previous scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }
